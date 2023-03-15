1a6c3aa10ca8c7fe514b6ff8301d5fe909d28fe0
Chris <CHRISTIANNY@LIVE.COM>
2023-03-15 17:43:23 -0300

    initial commit

---
+++ tres_crescentes.py
@@ -0,0 +1,17 @@
+def tres_c():
+    contents = []
+    while True:
+        try:
+            line = input()
+            contents.append(line)
+        except EOFError:
+            break
+        
+        #print(contents)
+        contents = [int(x) for x in contents]
+        contents = sorted(contents)
+        #print(contents)
+    
+    print (*contents, sep="\n")
+
+tres_c()
